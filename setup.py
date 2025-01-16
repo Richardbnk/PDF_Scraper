2ec41eda279280004d37da42b446214ed11f0e71
Richard Banak <45636763+Richardbnk@users.noreply.github.com>
2025-01-16 00:01:59 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setuptools.setup(
-    name='pdf_scrapping',
+    name='pdf_scraper',
     version='1.0.0',
     author="Richard Raphael Banak",
-    description="Tool to assist navigation using mouse and keyboard for automated actions when RPA techniques are not effective.",
-    url="https://github.com/Richardbnk/pdf_scrapping",
-    packages=['pdf_scrapping'],
+    description="Library for scrapping and RPA in PDF files.",
+    url="https://github.com/Richardbnk/pdf_scraper",
+    packages=['pdf_scraper'],
     
     py_modules = ['PyPDF2'],
     
